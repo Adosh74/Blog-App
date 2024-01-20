bec8018bdf0ff8d98e7df793551e8d4ebaec8ce9
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-20 08:36:10 +0200

    + add redis url to keys

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { DATABASE, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const { DATABASE, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIS_URL } =
+    process.env;
 
 module.exports = {
-  googleClientID: GOOGLE_CLIENT_ID,
-  googleClientSecret: GOOGLE_CLIENT_SECRET,
-  mongoURI: DATABASE,
-  cookieKey: '123123123',
+    googleClientID: GOOGLE_CLIENT_ID,
+    googleClientSecret: GOOGLE_CLIENT_SECRET,
+    mongoURI: DATABASE,
+    redisUrl: REDIS_URL,
+    cookieKey: '123123123',
 };
