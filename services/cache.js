204ac4fe1c556deb45205ba3556cd0ea071d35f0
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-02-24 22:07:20 +0200

    + complete cache functionality

--- services/cache.js
+++ services/cache.js
@@ -4,7 +4,24 @@ const keys = require('../config/keys');
 const client = new Redis(keys.redisUrl);
 
 const exec = mongoose.Query.prototype.exec;
+
+/**
+ *
+ * @returns {mongoose.Query}
+ */
+// *** This is a cache function that we are adding to the mongoose.Query.prototype ***
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
+    return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
     const key = JSON.stringify(
         Object.assign({}, this.getQuery(), {
             collection: this.mongooseCollection.name,
@@ -12,7 +29,7 @@ mongoose.Query.prototype.exec = async function () {
     );
 
     // +[1] see if we have a value for 'key' in redis
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     // +[2] if we do, return that
     if (cacheValue) {
@@ -24,7 +41,13 @@ mongoose.Query.prototype.exec = async function () {
     // +[3] otherwise, issue the query and store the result in redis
 
     const result = await exec.apply(this, arguments);
-    await client.set(key, JSON.stringify(result));
+    await client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 1000);
 
     return result;
+};
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    },
 };
